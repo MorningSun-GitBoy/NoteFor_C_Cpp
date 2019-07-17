f2f950bdb704f6f3fceb42820e6032a49c422e43
HuaWei-01 <AndyWolfcf2013@163.com>
2019-07-17 09:52:00 +0800

    correct spell fault

--- 001helloworld/Helloworld.cpp
+++ 001helloworld/Helloworld.cpp
@@ -1,10 +1,10 @@
 /*
-  实现控制台输出Hallo World!
+  实现控制台输出Hello World!
   通过实例了解C语言程序结构
 */
 #include <stdio.h>//引入C标准输入输出函数库
 int main(){//程序入口函数
-      printf("Hallo World!\n");//调用函数printf()实现功能
+      printf("Hello World!\n");//调用函数printf()实现功能
       return 0;//函数返回值
 }
 /*
@@ -12,4 +12,6 @@ int main(){//程序入口函数
   一个C程序包括 库引入、（主）函数、函数体组成
   函数定义返回值，并在函数体中实现返回
   函数体中可以用其他函数实现功能
+  程序输出为：
+  Hello World!
 */
--- 001helloworld/Helloworld_pp.cpp
+++ 001helloworld/Helloworld_pp.cpp
@@ -1,13 +1,13 @@
 /*
-  实现控制台输出Hallo World!
+  实现控制台输出Hello World!
   通过实例了解C++语言程序结构
 */
 #include <iostream>//引入标准流输入输出库
 #include <cstdio>//引入C标准输入输出函数库
 using namespace std;//指定命名空间std
 int main(){//主函数为程序入口
-      cout<<"Hallo World!"<<endl;//使用cout对象输出，以及对endl对象的运算实现换行
-      printf("Hallo World!\n");//调用函数printf()实现功能
+      cout<<"Hello World!"<<endl;//使用cout对象输出，以及对endl对象的运算实现换行
+      printf("Hello World!\n");//调用函数printf()实现功能
       return 0;//返回函数值
 }
 /*
@@ -16,8 +16,8 @@ int main(){//主函数为程序入口
   函数定义返回值，并在函数体中实现返回
   函数体中可以用对象实现功能,也可以调用其他函数实现
   程序输出为：
-  Hallo World!
-  Hallo World!
+  Hello World!
+  Hello World!
   
   在引入cstdio后也可以用printf实现相同的功能
   注意Cpp中引入库与C不同，不用添加.h后缀
